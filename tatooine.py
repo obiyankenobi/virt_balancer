46baca5098d4fd35bcc1f9528085d58893335221
Pedro Ferreira <pedroferreira@vialink.com.br>
2013-05-17 10:45:14 -0300

    Corrigindo bugs após primeiros testes

--- tatooine.py
+++ tatooine.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import socket
 import threading
@@ -9,14 +10,49 @@ from collections import OrderedDict
 
 UDP_PORT = 11998
 addresses = []
+#addresses = ['Aa', 'Bb', 'Cc', 'Dd', 'Ee']
 # Used at run method of migration thread to compare if all INFO packet have already arrived
 addressesSet = set(addresses)
 
 pmInfo = {}
+#pmInfo = {
+#       'Aa': {
+#           'cpu': 15,
+#           'mem': 30,
+#           'network': 50,
+#       },
+#       'Bb': {
+#           'cpu': 98,
+#           'mem': 90,
+#           'network': 10,
+#       },
+#       'Cc': {
+#           'cpu': 60,
+#           'mem': 30,
+#           'network': 60,
+#       },
+#       'Dd': {
+#           'cpu': 70,
+#           'mem': 70,
+#           'network': 70,
+#       },
+#       'Ee': {
+#           'cpu': 40,
+#           'mem': 5,
+#           'network': 10,
+#       },
+#   }
 
 # Quando temos mais de uma mv para migrar, vamos considerar que escolhida a MF destino da primeira MV, a segunda não pode escolher a mesma MF?
 # Essa dict garante essa condição
 receiveMigration = {}
+#receiveMigration = {
+#    'Aa': True,
+#    'Bb': True,
+#    'Cc': True,
+#    'Dd': True,
+#    'Ee': True,
+#}
 
 # Used to calculate VM image (MEM_TOT*mem/100.0)
 MEM_TOT = 4096
@@ -39,7 +75,7 @@ def main():
         data, (addr, port) = sock.recvfrom(32768)
         packet = Packet.deserialize(data)
         if packet.header.packetType == Packet.VM_INFO:
-            migration = Migration(addr, packet.data.vmDict)
+            migration = Migration(addr, packet.data.vmDict, sock)
             migration.start()
 
 
@@ -47,24 +83,26 @@ class Migration(threading.Thread):
     """ Analyze a physical machine that needs migration
     """
 
-    def __init__(self, addr, vmInfo):
+    def __init__(self, addr, vmInfo, socket):
         threading.Thread.__init__(self, name='Migration')
-        address = addr
-        vmInfo = vmInfo
-        migrated = False
-        costDict = {}
-        addrReceived = set()
+        self.address = addr
+        self.vmInfo = vmInfo
+        self.migrated = False
+        self.costDict = {}
+        self.addrReceived = set()
+        self.sock = socket
 
 
     def run(self):
+        #print 'Run'
         pktHeader = PacketHeader(Packet.SEND_INFO)
         pkt = Packet(pktHeader, None)
         # Send packet to physical machines requesting their usage informations
         for a in addresses:
-            sock.sendto(pkt.serialize(), (a, UDP_PORT))
+            self.sock.sendto(pkt.serialize(), (a, UDP_PORT))
         # Receive information packets and check if have already received all
         while addrReceived != addressesSet:
-            data, (addr, port) = sock.recvfrom(32768)
+            data, (addr, port) = self.sock.recvfrom(32768)
             packet = Packet.deserialize(data)
             if packet.header.packetType == Packet.INFO:
                 addrReceived.add(addr)
@@ -72,12 +110,13 @@ class Migration(threading.Thread):
                 pmInfo[addr]['cpu'] = packet.data.cpu
                 pmInfo[addr]['mem'] = packet.data.mem
                 pmInfo[addr]['network'] = packet.data.network
-        migration.analyzeMigration()
+        self.analyzeMigration()
 
 
     def analyzeMigration(self):
-        for k, v in vmInfo.items():
-            self.costDict[k] = costVM(v['cpu'], v['mem'], v['network'], MEM_TOT*v['mem']/100.0)
+        #print 'Analyze'
+        for k, v in self.vmInfo.items():
+            self.costDict[k] = self.costVM(v['cpu'], v['mem'], v['network'], MEM_TOT*v['mem']/100.0)
 
         arrayMV = []
         self.costDict = OrderedDict(sorted(self.costDict.items(), key=lambda x: x[1]))
@@ -94,14 +133,14 @@ class Migration(threading.Thread):
 
 
     def volumeVM(self, cpu, mem, network):
-        return (1.0/(1-cpu))*(1.0/(1-mem))*(1.0/(1-network))
+        return (1.0/(1-(cpu/100.0)))*(1.0/(1-(mem/100.0)))*(1.0/(1-(network/100.0)))
 
 
     def costVM(self, cpu, mem, network, img):
         return (self.volumeVM(cpu, mem, network)**alfa)*(img**beta)
 
 
-    def relievePM(self, resource, address):
+    def relievePM(self, resource):
         # Resource is an array of tuples, used when the algorithm have to migrate more than one VM,
         # so we must check if the migration of all VM's will relieve the PM
         cpuTot = 0
@@ -112,7 +151,14 @@ class Migration(threading.Thread):
             memTot += r[1]
             networkTot += r[2]
 
-        if pmInfo[address]['cpu'] + cpuTot < 85 and pmInfo[address]['mem'] + memTot < 85 and pmInfo[address]['network'] + networkTot < 85:
+        if pmInfo[self.address]['cpu'] - cpuTot < 85 and pmInfo[self.address]['mem'] - memTot < 85 and pmInfo[self.address]['network'] - networkTot < 85:
+            return True
+        return False
+
+
+    def canReceiveVM(self, cpu, mem, network, address):
+        # We assume that one physical machine can receive only one migrated virtual machine
+        if pmInfo[address]['cpu'] + cpu < 85 and pmInfo[address]['mem'] + mem < 85 and pmInfo[address]['network'] + network < 85:
             return True
         return False
 
@@ -121,7 +167,7 @@ class Migration(threading.Thread):
         # Chosen defines the physical machine that will receive the virtual machine migration
         chosen = {'addr': None, 'usage': 0}
         for k, v in pmInfo.items():
-            if self.relievePM([(cpu, mem, network)], k) and receiveMigration[k]:
+            if self.canReceiveVM(cpu, mem, network, k) and receiveMigration[k]:
                 usage = v['cpu'] + v['mem'] + v['network'] + cpu + mem + network
                 if usage > chosen['usage']:
                     chosen = {'addr': k, 'usage': usage}
@@ -130,6 +176,7 @@ class Migration(threading.Thread):
 
     def migrate(self, dataMigration):
         # data_migration is an array of tuples like (addrDest, vmName)
+        #print 'Migrate   ', dataMigration
         pktHeader = PacketHeader(Packet.MIGRATE)
         for d in dataMigration:
             migrateDict = {
@@ -137,7 +184,7 @@ class Migration(threading.Thread):
             }
             pktData = PacketMigrate(migrateDict)
             packet = Packet(pktHeader, pktData)
-            sock.sendto(packet.serialize(), (self.addr, UDP_PORT))
+            self.sock.sendto(packet.serialize(), (self.address, UDP_PORT))
 
 
     def getDataMigration(self, arrayMV):
@@ -148,12 +195,13 @@ class Migration(threading.Thread):
                 # Check if this combination relieves the physical machine, if True find destination for them
                 cost = []
                 for vm in c:
-                    cost.append((self.costDict[vm]['cpu'], self.costDict[vm]['mem'], self.costDict[vm]['network']))
-                if self.relievePM(cost, self.address):
+                    cost.append((self.vmInfo[vm]['cpu'], self.vmInfo[vm]['mem'], self.vmInfo[vm]['network']))
+                if self.relievePM(cost):
+                    #print 'Relieve', cost
                     addrDest = []
                     willMigrate = True
                     for vm in c:
-                        addr = self.findDestination(self.costDict[vm]['cpu'], self.costDict[vm]['mem'], self.costDict[vm]['network'])
+                        addr = self.findDestination(self.vmInfo[vm]['cpu'], self.vmInfo[vm]['mem'], self.vmInfo[vm]['network'])
                         if addr:
                             addrDest.append(addr)
                             receiveMigration[addr] = False
